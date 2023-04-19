186d03047af47918c288ca919f657836061d4eb3
Ario <ariowistus@gmail.com>
2023-04-19 13:38:28 +0200

    model Rata

--- src/com/company/service/RateCalculationServiceImpl.java
+++ src/com/company/service/RateCalculationServiceImpl.java
@@ -13,6 +13,32 @@ public class RateCalculationServiceImpl implements RateCalculationService {
     @Override
     public List<Rate> calculate(InputData inputData) {
 
+        List<Rate> rates = new LinkedList<>();
+
+        BigDecimal rateNumber = BigDecimal.ONE;
+
+        Rate firstRate = calcolateFirstRate(rateNumber, inputData);
+
+        rates.add(firstRate);
+        Rate previousRate =firstRate;
+
+        for(
+                BigDecimal index = rateNumber.add(BigDecimal.ONE);
+                index.compareTo(inputData.getMonthsDuration()) <=0;
+                index=index.add(BigDecimal.ONE)
+        ){
+            Rate nextRate = calculateNextRate(index, inputData, previousRate);
+            rates.add(nextRate);
+            previousRate = nextRate;
+        }
+        return rates;
+    }
+
+    private Rate calcolateFirstRate(BigDecimal rateNumber, InputData inputData) {
+        return null;
+    }
+
+    private Rate calculateNextRate(BigDecimal index, InputData inputData, Rate previousRate) {
         return null;
     }
 }
